63a316d86913923b9ed77c854610aacec8d26691
Alexander Savushkin <74372739+avsavushkiny@users.noreply.github.com>
2023-03-07 03:06:38 +0300

    upd 070323 1

--- pico2040/mg_os/sys_trm.h
+++ pico2040/mg_os/sys_trm.h
@@ -16,33 +16,24 @@ class InputOutput
   private:
   public:
 
-  String getString(String oText)
-  {
-    if (Serial.available() > 0)
-    {
-      Serial.println(oText);
-      return Serial.readString();
-    }
-    return "NULL";
-  }
-
 } io;
 
 void terminalInputData()
 {
-  if (io.getString("Command: ") == "--")
+  String data{};
+
+  if (Serial.available() > 0)
   {
-    if (io.getString("SYS") == "MCU")
+    data = Serial.readString(); data.trim();
+    if (data == "--")
     {
-      Serial.println("RASPBERRY_PICO_2040_EN");
-    }
-    else if (io.getString("SYS") == "GFX")
-    {
-      Serial.println("ST7565_ERC12864_F_4W_SW_SPI");
+      if (data == "sys")
+      {
+        Serial.println("RASPBERRY_PICO_2040_EN");
+      }
     }
     else Serial.println("Non command");
   }
-  else Serial.println("Non command");
 }
 
 void terminalGfx()
